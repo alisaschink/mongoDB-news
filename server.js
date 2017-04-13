2dea9be8b2bf86ee5c87b34463286fb04afcf3fa
Alisa Schink <Alisa Schink>
2017-04-13 17:54:32 -0400

    scrapes news successfully

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 // Snatches HTML from URLs
 var request = require('request');
-// Scrapes our HTML
+// Scrapes HTML
 var cheerio = require('cheerio');
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Requires userModel model
+var Example = require("./userModel.js");
+
+// Initialize Express
+var app = express();
+
+
+// Use morgan and body parser
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+
+// Database configuration with mongoose
+//mongodb://heroku_p914858m:m3nc3lj1dqhp01dr87ob8fim27@ds031895.mlab.com:31895/heroku_p914858m
+mongoose.connect("mongodb://localhost/week18day3mongoose");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
 
 // Make a request call to grab the HTML body from the site of your choice
 request('http://www.huffingtonpost.com/', function (error, response, html) {
@@ -10,12 +50,10 @@ request('http://www.huffingtonpost.com/', function (error, response, html) {
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(html);
 
-  // An empty array to save the data that we'll scrape
+  // An empty array to save the data scraped data
   var result = [];
 
   // Select each instance of the HTML body that you want to scrape
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors, 
-  // but be sure to visit the package's npm page to see how it works
   $('h2.card__headline').each(function(i, element){
 
     var link = $(element).children().attr("href");
@@ -28,4 +66,49 @@ request('http://www.huffingtonpost.com/', function (error, response, html) {
     });
     });
   console.log(result);
+});
+
+
+// Routes
+// ======
+
+app.post("/submit", function(req, res) {
+
+  var user = new Example(req.body);
+
+
+/* OUR CUSTOM METHODS
+ * (methods created in the userModel.js)
+ * -/-/-/-/-/-/-/-/-/ */
+
+
+ // TODO: Use our custom methods to create
+ // the FullName and LastUpdatedDate entries
+ // in the doc that the user submits. These
+ // will get saved to the mongoDB collection.
+ //
+ // TIP: You must create these methods in the model.
+user.getFullName();
+user.LastUpdatedDate();
+
+// END OF CUSTOM METHODS
+// =====================
+// NORMAL METHOD BELOW
+
+  // Save a user to our mongoDB
+  user.save(function(error, doc) {
+    // Send an error to the browser
+    if (error) {
+      res.send(error);
+    }
+    // Or send the doc to our browser
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
---
+++ userModel.js
@@ -0,0 +1,85 @@
+// Require mongoose
+var mongoose = require("mongoose");
+
+// Create the Schema class
+var Schema = mongoose.Schema;
+
+// new Schema: UserSchema
+var UserSchema = new Schema({
+  // firstName: a trimmed, required string
+  firstName: {
+    type: String,
+    trim: true,
+    required: "First Name is Required"
+  },
+  // lastName: a trimmed, required string
+  lastName: {
+    type: String,
+    trim: true,
+    required: "Last Name is Required"
+  },
+  // username: a trimmed, required string
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is Required"
+  },
+  // password: a trimmed, required string that must be more than 6 chars
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer."
+    ]
+  },
+  // email: a string that must match an email format and must be unique in the collection
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
+  },
+  // userCreated: the current date
+  userCreated: {
+    type: Date,
+    default: Date.now
+  },
+  // lastUpdated: a date type entry
+  lastUpdated: { type: Date },
+  // fullName: a string type entry
+  fullName: String
+});
+
+
+/* vvv MAKE YOUR CUSTOM METHODS HERE vvv */
+/* ===================================== */
+
+// 1. getFullName: concatenate the first and last name and return it
+  UserSchema.methods.getFullName = function() {
+    // enter your method code here
+    var first = this.firstName;
+    var last = this.lastName;
+    var name = first + " " + last;
+    this.fullName = name;
+
+    return this.fullName
+  };
+
+// 2. lastUpdatedDate: save the current date to a variable and return it
+  UserSchema.methods.lastUpdatedDate = function() {
+    // enter your method code here
+    this.lastUpdated = date.now();
+
+    return this.lastUpdated;
+   
+  };
+
+
+// Use the above schema to make the User model
+var User = mongoose.model("User", UserSchema);
+
+// Export the model so the server can use it
+module.exports = User;
