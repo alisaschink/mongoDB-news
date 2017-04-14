aaffb39569de83e1ac495f50750258cdc9734440
Alisa Schink <Alisa Schink>
2017-04-14 13:27:35 -0400

    views and controller set up

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ var cheerio = require('cheerio');
 mongoose.Promise = Promise;
 
 // Requires userModel model
-var Example = require("./userModel.js");
+// var Example = require("./userModel.js");
+
+var newsController = require('./controllers/newsController');
+
 
 // Initialize Express
 var app = express();
@@ -43,38 +46,24 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-// Make a request call to grab the HTML body from the site of your choice
-request('http://www.huffingtonpost.com/', function (error, response, html) {
+//set up handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
+app.use('/', newsController);
 
-  // An empty array to save the data scraped data
-  var result = [];
 
-  // Select each instance of the HTML body that you want to scrape
-  $('h2.card__headline').each(function(i, element){
-
-    var link = $(element).children().attr("href");
-    var title = $(element).children().text();
-
-    // Save these results in an object that we'll push into the result array we defined earlier
-    result.push({
-      title: title,
-      link: link
-    });
-    });
-  console.log(result);
-});
 
 
 // Routes
 // ======
 
-app.post("/submit", function(req, res) {
+// app.post("/submit", function(req, res) {
 
-  var user = new Example(req.body);
+//   var user = new Example(req.body);
 
 
 /* OUR CUSTOM METHODS
@@ -88,25 +77,25 @@ app.post("/submit", function(req, res) {
  // will get saved to the mongoDB collection.
  //
  // TIP: You must create these methods in the model.
-user.getFullName();
-user.LastUpdatedDate();
+// user.getFullName();
+// user.LastUpdatedDate();
 
 // END OF CUSTOM METHODS
 // =====================
 // NORMAL METHOD BELOW
 
   // Save a user to our mongoDB
-  user.save(function(error, doc) {
-    // Send an error to the browser
-    if (error) {
-      res.send(error);
-    }
-    // Or send the doc to our browser
-    else {
-      res.send(doc);
-    }
-  });
-});
+//   user.save(function(error, doc) {
+//     // Send an error to the browser
+//     if (error) {
+//       res.send(error);
+//     }
+//     // Or send the doc to our browser
+//     else {
+//       res.send(doc);
+//     }
+//   });
+// });
 
 // Listen on port 3000
 app.listen(3000, function() {
